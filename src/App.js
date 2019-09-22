1cafbc498c0e3f7a07d56bf73c8d49b25dcff4bb
kgonjo <kgonjo@yahoo-corp.jp>
2019-09-23 00:20:54 +0900

    import material-ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from '@material-ui/core';
 import Header from './components/Header';
 import Todo from './containers/Todo';
 import Home from './containers/Home';
@@ -9,11 +10,13 @@ const App = () => {
   return (
     <Router>
       <div>
-        <Header />
-
-        <Route exact path="/" component={Home} />
-        <Route path="/list" component={List} />
-        <Route path="/todo" component={Todo} />      
+        <Container fixed>
+          <Header />
+        
+          <Route exact path="/" component={Home} />
+          <Route path="/list" component={List} />
+          <Route path="/todo" component={Todo} />
+        </Container>
       </div>
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const header = () => (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/list">List</Link>
-      </li>
-      <li>
-        <Link to="/todo">Todo</Link>
-      </li>
-    </ul>
-);
+const styles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const header = () => {
+  
+  const classes = styles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" className={classes.title}>
+             Todo App
+          </Typography>
+          <Link to="/"><Button color="inherit">Home</Button></Link>
+          <Link to="/list"><Button color="inherit">List</Button></Link>
+          <Link to="/todo"><Button color="inherit">Todo</Button></Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
 
 export default header;
